32426c7323b856a5728b96babfb4a4c2fb427612
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2023-11-30 12:30:50 +0530

    recursion practice

--- recursionassin1.py
+++ recursionassin1.py
@@ -46,9 +46,64 @@
 
 # print first n even no in reverse
 
-def printn(n):
-    if n>0:
-        print(2*n)
-        printn(n-1)
+# def printn(n):
+#     if n>0:
+#         print(2*n)
+#         printn(n-1)
 
-r=printn(10)
+# r=printn(10)
+
+# sum of first n natural numbers
+
+# def printn(n):
+#     if n == 1:
+#         return 1
+#     s=n+printn(n-1)
+#     return s
+
+# r=printn(5)
+# print(r)
+
+#sum of n odd no
+
+# def sum(n):
+#     if n == 1:
+#         return 1
+#     return 2*n-1 + sum(n-1)
+
+# r=sum(6)
+# print(r)
+
+# sum of first n even no
+
+# def sum(n):
+#     if n == 1:
+#         return 2
+#     s=2*n + sum(n-1)
+#     return s
+
+# r=sum(5)
+# print(r)
+
+
+# calculate factorial
+
+# def fac(n):
+#     if n == 0:
+#         return 1
+#     s= n* fac(n-1)
+#     return s
+
+# r=fac(3)
+# print(r)
+
+# sum of square of n no
+
+def sum(n):
+    if n == 1:
+        return 1
+    s= n*n + sum(n-1)
+    return s
+
+r=sum(2)
+print(r)
